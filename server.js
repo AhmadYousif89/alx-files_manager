53d358ed382928cc66302e78e0b6fa8b00b19e4d
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-28 14:21:46 +0300

    Add server.js file with basic setup

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import appRoutes from './routes/index';
+import { errorHandler } from './middlewares/errors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(appRoutes);
+app.use((req, res) => {
+  res.status(404).send({ error: 'Not found' });
+});
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
