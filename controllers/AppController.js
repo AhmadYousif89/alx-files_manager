b3c95c473219c3ab5ad5e50a12c6de56c449b62f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-28 14:23:08 +0300

    Add AppController with getStatus and getStats functions

---
+++ controllers/AppController.js
@@ -0,0 +1,15 @@
+import mongoDB from '../utils/db';
+import redisClient from '../utils/redis';
+import asyncWrapper from '../middlewares/async_wrapper';
+
+// GET /api/v1/status
+export const getStatus = (_req, res) => {
+  res.status(200).json({ redis: redisClient.isAlive(), db: mongoDB.isAlive() });
+};
+
+// GET /api/v1/stats
+export const getStats = asyncWrapper(async (_req, res) => {
+  const users = await mongoDB.nbUsers();
+  const files = await mongoDB.nbFiles();
+  res.status(200).json({ users, files });
+});
