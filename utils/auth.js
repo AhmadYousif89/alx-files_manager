9d362e8a843fc44aec9227c0c9cbfbfeeeae2ad0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-29 23:04:59 +0300

    Refactor getUserFromHeader to return only the id and email of the user

--- utils/auth.js
+++ utils/auth.js
@@ -8,7 +8,8 @@ import mongoDB from './db';
  * Retrieves the user based on the x-token header.
  * @param {Request} req - The Express request object.
  * @throws {ApiError} - Throws an error if the token is invalid or user not found.
- * @returns {Object} - Returns the user object if the token is valid.
+ * @returns {Promise<{_id: ObjectId; email: string}> | null} - The user object if
+ * the token is valid and user is found. Otherwise, returns null.
  */
 const getUserFromHeader = async (req) => {
   const token = req.headers['x-token'];
@@ -18,7 +19,10 @@ const getUserFromHeader = async (req) => {
   const userId = await redisClient.get(key);
   if (!userId) throw new ApiError(401, 'Unauthorized');
 
-  const user = await mongoDB.users.findOne({ _id: ObjectId(userId) });
+  const user = await mongoDB.users.findOne(
+    { _id: ObjectId(userId) },
+    { projection: { email: 1 } },
+  );
   if (!user) throw new ApiError(401, 'Unauthorized');
 
   return user;
