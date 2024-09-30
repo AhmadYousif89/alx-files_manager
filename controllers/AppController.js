ed67cf066b0ef0df2978dcf1d2e1cc9574d760cf
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-30 22:42:16 +0300

    Refactor AppController.js for better response handling

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,13 +3,13 @@ import redisClient from '../utils/redis';
 import asyncWrapper from '../utils/async_wrapper';
 
 // GET /api/v1/status
-export const getStatus = (_req, res) => {
-  res.status(200).json({ redis: redisClient.isAlive(), db: mongoDB.isAlive() });
+export const getStatus = (_, res) => {
+  return res.status(200).json({ redis: redisClient.isAlive(), db: mongoDB.isAlive() });
 };
 
 // GET /api/v1/stats
-export const getStats = asyncWrapper(async (_req, res) => {
+export const getStats = asyncWrapper(async (_, res) => {
   const users = await mongoDB.nbUsers();
   const files = await mongoDB.nbFiles();
-  res.status(200).json({ users, files });
+  return res.status(200).json({ users, files });
 });
