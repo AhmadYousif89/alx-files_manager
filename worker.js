52c21fc5d664df6872054062c64c81938cd2378f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-09-30 16:06:47 +0300

    Refactor image thumbnail creation and add user welcome email

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
-import Queue from 'bull';
-import { promises as fs } from 'fs';
-import imageThumbnail from 'image-thumbnail';
+import sharp from 'sharp';
+import Queue from 'bull/lib/queue';
 import { ObjectId } from 'mongodb';
 import mongoDB from './utils/db';
 
 const fileQueue = new Queue('fileQueue');
+const userQueue = new Queue('emailQueue');
 
 /**
  *
@@ -13,30 +13,75 @@ const fileQueue = new Queue('fileQueue');
  * @returns {Promise<void>} A promise that resolves when the thumbnail is created
  */
 const createThumbnail = async (filePath, size) => {
-  const thumbnail = await imageThumbnail(filePath, { width: size });
-  const thumbnailPath = `${filePath}_${size}.png`;
-  return fs.writeFile(thumbnailPath, thumbnail);
+  try {
+    const thumbnailPath = `${filePath}_${size}.png`;
+    const thumbnail = await sharp(filePath)
+      .resize(size, size, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+      .toFile(thumbnailPath);
+    console.log(
+      `New image created with dimensions: ${thumbnail.width}x${thumbnail.height}`,
+    );
+    console.log(thumbnail);
+  } catch (error) {
+    console.error(`Error creating thumbnail of size ${size}:`, error);
+  }
 };
 
 fileQueue.process(async (job) => {
-  const { fileId, userId } = job.data;
+  try {
+    const { fileId, userId } = job.data;
 
-  if (!fileId) {
-    throw new Error('Missing fileId');
+    if (!fileId) {
+      throw new Error('Missing fileId');
+    }
+
+    if (!userId) {
+      throw new Error('Missing userId');
+    }
+
+    const filter = { _id: ObjectId(fileId), userId: ObjectId(userId) };
+    const file = await mongoDB.files.findOne(filter);
+    if (!file) {
+      throw new Error('File not found');
+    }
+
+    const filePath = file.localPath;
+    const { width, height } = await sharp(filePath).metadata();
+    console.log(`Original image dimensions: ${width}x${height}`);
+    const sizes = [100, 250, 500];
+    const thumbnailPromises = sizes.map((size) => createThumbnail(filePath, size));
+    await Promise.allSettled(thumbnailPromises);
+  } catch (error) {
+    console.error('Error processing image:', error);
+    throw error; // Re-throw the error to mark the job as failed
   }
+});
 
-  if (!userId) {
-    throw new Error('Missing userId');
+userQueue.process(async (job) => {
+  const { userId } = job.data;
+
+  if (!userId) throw new Error('Missing userId');
+
+  const user = await mongoDB.users.findOne({ _id: ObjectId(userId) });
+  if (!user) throw new Error('User not found');
+
+  console.log(`Welcome ${user.email}!`);
+  try {
+    const creator = 'Ahmad Yousif';
+    const subject = 'The Files Manager API';
+    const href = 'https://github.com/ahmadyousif89';
+    const content = `
+      <main>
+        <h1>Hello ${user.email},</h1>
+        <p>
+          Welcome to the <a href="${href}/alx-files_manager">Files Manager API</a>,
+          a file management API built with Node and Express.js by
+          <a href="${href}">${creator}</a>.
+        </p>
+      </main>
+    `;
+    // Mailer.sendMail(Mailer.buildMessage(user.email, subject, content));
+  } catch (err) {
+    console.error(err);
   }
-
-  const filter = { _id: ObjectId(fileId), userId: ObjectId(userId) };
-  const file = await mongoDB.files.findOne(filter);
-  if (!file) {
-    throw new Error('File not found');
-  }
-
-  const filePath = file.localPath;
-  const sizes = [500, 250, 100];
-  const thumbnailPromises = sizes.map((size) => createThumbnail(filePath, size));
-  await Promise.all(thumbnailPromises);
 });
